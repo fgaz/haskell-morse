e59ec2dc943e090c498aa1e4c16d2b1162991b02
Francesco Gazzetta <fgaz@users.noreply.github.com>
2014-08-14 13:50:09 +0200

    foldl powagit add morse.hs joe morse.hsgit add morse.hs !

--- morse.hs
+++ morse.hs
@@ -7,10 +7,23 @@ decWith (_:tree) (x:xs) | x=='.' = decWith fstHalf xs
                       (fstHalf, sndHalf) = splitAt half tree
 --decWith _ _ = error "La lettera è troppo lunga!"
 
-dec = decWith "_eisuarwtndkmgo" -- per ora solo le lettere formate da max 3 simboli
+dec = decWith " eisuarwtndkmgo" -- per ora solo le lettere formate da max 3 simboli
 
 decWord [] = ""
 decWord "/" = ""
 decWord xs | '/' `elem` xs = let (letter, (_:word)) = break (=='/') xs in
                                dec letter : decWord word
            | otherwise = [dec xs]
+
+
+--or with foldl:
+
+dec' :: String -> Char
+dec' x = head $ foldl f "_eisuarwtndkmgo" x
+
+f :: String -> Char -> String
+f (_:acc) x | x=='.' = fstHalf
+            | x=='-' = sndHalf
+            where
+              half = length acc `div` 2
+              (fstHalf, sndHalf) = splitAt half acc
