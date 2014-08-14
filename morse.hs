c39996370bcb468f97ae0d1bb763423bb41e0d37
Francesco Gazzetta <fgaz@users.noreply.github.com>
2014-08-14 14:19:44 +0200

    Note to self: do not re-implement fold and map with obscure functions in every program without noticing

--- morse.hs
+++ morse.hs
@@ -1,25 +1,5 @@
 import Data.List.Split
 
-decWith (x:_)  [] = x
-decWith (_:tree) (x:xs) | x=='.' = decWith fstHalf xs
-                  | x=='-' = decWith sndHalf xs
---                  | otherwise = error $ "Trovati simboli diversi da \".\" o \"-\"!"
-                  where
-                      half = length tree `div` 2
-                      (fstHalf, sndHalf) = splitAt half tree
---decWith _ _ = error "La lettera è troppo lunga!"
-
-dec = decWith " eisuarwtndkmgo" -- per ora solo le lettere formate da max 3 simboli
-
-decWord [] = ""
-decWord "/" = ""
-decWord xs | '/' `elem` xs = let (letter, (_:word)) = break (=='/') xs in
-                               dec letter : decWord word
-           | otherwise = [dec xs]
-
-
---or with foldl:
-
 dec' :: String -> Char
 dec' x = head $ foldl f " eisuarwtndkmgo" x
 
