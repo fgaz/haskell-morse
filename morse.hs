af45f17a3179502ec25434e6f7461c04fb13b1f1
Francesco Gazzetta <fgaz@users.noreply.github.com>
2014-08-30 11:44:43 +0200

    added encoder

--- morse.hs
+++ morse.hs
@@ -1,4 +1,5 @@
 import Data.List.Split
+import qualified Data.List as L
 
 dec :: String -> Char
 dec letter = head $ foldl traverseTree " eishvuf?arl?wpjtndbxkcymgzqo??" letter
@@ -12,3 +13,18 @@ traverseTree (_:tree) selector | selector=='.' = fstHalf
 
 decWord :: String -> String
 decWord word = map dec $ splitOn "/" word
+
+
+encWith :: String -> Char -> String
+encWith (x:tree) letter | letter == x = ""
+                        | letter `elem` fstHalf = '.':(encWith fstHalf letter)
+                        | letter `elem` sndHalf = '-':(encWith sndHalf letter)
+                        where
+                          half = length tree `div` 2
+                          (fstHalf, sndHalf) = splitAt half tree
+
+enc :: Char -> String
+enc = encWith " eishvuf?arl?wpjtndbxkcymgzqo??"
+
+encWord :: String -> String
+encWord word = L.intercalate "/" $ map enc word
